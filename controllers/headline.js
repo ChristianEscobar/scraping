42eca1d672cfbb40a1fc3c20d1293a3a373f63db
Christian Escobar <crs.escobar@gmail.com>
2018-03-22 22:34:11 -0700

    home page updates

--- controllers/headline.js
+++ controllers/headline.js
@@ -4,7 +4,6 @@ const models = require("../models");
 module.exports.headlines = function(req, res) {
 	return models.Headline.find({})
 	.then((dbHeadlines) => {
-		console.log("return===>", dbHeadlines);
 
 		const headlinesObj = {
 			headlines: dbHeadlines
