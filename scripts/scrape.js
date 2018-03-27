16173cee90ee815a52b7756df1412e6ca213c686
Christian Escobar <crs.escobar@gmail.com>
2018-03-26 22:42:22 -0700

    fixed scraping

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,10 +1,12 @@
+"use strict"
+
 const cheerio = require("cheerio");
 const request = require("request");
 // Load Mongo Models
 const models = require("../models");
 const mongoose = require("mongoose");
 
-const url = "https://gizmodo.com/"
+const url = "https://gizmodo.com/";
 
 module.exports.execute = function(callbackFn) {
 
@@ -24,31 +26,32 @@ module.exports.execute = function(callbackFn) {
 
       // Skip over ads
       if(link.startsWith("https://deals.kinja.co")) {
-        return true;
-      }
+        return false;
+      } else {
+        const headline = $(element).children().text();
 
-      const headline = $(element).children().text();
-      const summary = $(element).parent().siblings("div.item__content").children("div.entry-summary").text();
+        const summary = $(element).parent().siblings("div.item__content").children("div.entry-summary").text();
       
-      // Save article data to the database
-      const article = {
-        headline: headline,
-        summary: summary,
-        link: link
-      }
+        // Save article data to the database
+        const article = {
+          headline: headline,
+          summary: summary,
+          link: link
+        }
 
-      articles.push(article);
-      
+        articles.push(article);
+      }
     });
 
-    models.Headline.create(articles)
-      .then((dbArticles) => {
+    if(articles.length > 0) {
+
+      models.Headline.create(articles)
+      .then( dbArticles => {
         console.log("Articles saved!");
         
         callbackFn({articles: dbArticles});
       })
-      .catch((error) => {
-        console.error(error);
-      });  
+      .catch( error => console.error(error));
+    }
   });
 }
